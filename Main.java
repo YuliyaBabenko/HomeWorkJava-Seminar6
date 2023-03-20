f3420c1d8db4ee8afe7ccc88d14fd16a734506ee
Yuliya <ju7iab@gmail.com>
2023-03-20 21:17:39 +0300

    final

---
+++ Main.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+// Учитывая двоичного дерева, верните порядок обхода значений его узлов.
+// пример
+// Input: root = [1,null,2,3]
+// Output: [1,2,3]
+
+public class Main {
+    public static void main(String[] args) {
+        task();
+    }
+
+    private static void task() {
+        TreeNode tNode = new TreeNode(1,null, new TreeNode(2,null,new TreeNode(3)));
+        System.out.println(preorderTraversal(tNode));
+    }
+
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root != null) {
+            list.add(root.val);
+            list.addAll(preorderTraversal(root.left));
+            list.addAll(preorderTraversal(root.right));
+        }
+        return list;
+    }
+
+    public void fillTree(TreeNode root, Integer in) {
+        if (root == null) {
+            root.val = in;
+        } else if (root.left == null) {
+            fillTree(root.left, in);
+        } else {
+            fillTree(root.right, in);
+        }
+    }
+}
